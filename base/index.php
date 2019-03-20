d39ced4e0e7a44b69a5f3f861ac2c2d06670fbe3
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-03-19 20:49:42 -0600

    Operaciones en PHP

--- base/index.php
+++ base/index.php
@@ -15,21 +15,67 @@
         <h1>Aprendiendo PHP</h1>
 
         <div class="contenido">
+
             <?php
 
-            $titulo = "Aprendiendo PHP";
-
+            echo "10+20 = " . (10 + 20);
             ?>
-
-            <p><?php echo $titulo; ?></p>
-
+            <hr>
             <?php
-              /***************   *** Comentario *** ***************/
-              /* @Descripcion:  Varias lineas
-              /* @Acción     : 
-              /***************   *** ********** *** ***************/ 
-               
-              //Comentario en una sola linea.
+
+            $numero1 = 10;
+            $numero2 = 20;
+            $numero3 = 30;
+            $numero4 = 40;
+
+            //Suma
+
+            echo $numero1 + $numero2;
+
+            //Resta
+            echo "<hr>";
+            echo $numero4 - $numero3;
+
+            //Multiplicación
+            echo "<hr>";
+            echo $numero1 * $numero2;
+
+            echo "<hr>";
+            //División;
+            echo $numero4 / $numero1;
+
+            echo "<hr>";
+            echo ($numero1 + $numero2) * ($numero1 * .16);
+
+            //incremento por 1 
+            echo "<hr>";
+            $numero1++;
+            echo $numero1;
+
+            //decremento por 1 
+            echo "<hr>";
+            $numero1--;
+            echo $numero1;
+
+            //incremento en la misma variable
+            $total = 0;
+            $total += 10;
+            $total += 20;
+
+            echo "<hr>";
+            echo $total;
+
+            $total = 100;
+            $total -= 10;
+            $total -= 20;
+            $total -= 10;
+
+            echo "<hr>";
+            echo $total;
+
+            //Cuanto queda en la division 20/3 
+            echo "<hr>";
+            echo 20 % 3;
             ?>
 
 
