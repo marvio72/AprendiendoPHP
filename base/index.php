d2caf2556fb2e6e9e891e602113c2b1b03af60ec
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-03-19 22:44:59 -0600

    Agregando y quitando nuevos valores a un array

--- base/index.php
+++ base/index.php
@@ -17,28 +17,58 @@
         <div class="contenido">
 
             <?php
-              // versiones anteriores
-               $tecnologias = ['CSS','HTML','JavaScript','jQuery'];
-               //print_r($tecnologias);
-              // nuevas versiones
-            ?>
-              <pre><?php print_r($tecnologias);?></pre>
-              
-            <?php  
-              echo "<hr>";
-              $lenguajes = array('CSS','JQuery','HTML','JavaScript','JNode','PHP',20);
-              //echo $lenguajes[4];
+
+            $tecnologias = ['CSS', 'HTML', 'JavaScript', 'jQuery'];
             ?>
 
+            <pre><?php print_r($tecnologias); ?></pre>
+            <!-- Incrementamos un elemento al final del arreglo -->
+            <?php $tecnologias[] = 'Pyton'; ?>
+            <?php $tecnologias[] = 'Java'; ?>
+
+            <pre><?php print_r($tecnologias); ?></pre>
+
+            <?php  ?>
+            <?php $java = array_pop($tecnologias); ?>
+            <h1><?php echo $java; ?></h1>
+
             <pre>
-              <?php var_dump($lenguajes); ?>
+              <?php print_r($tecnologias); ?>
             </pre>
+
+            <?php
+               // Borrar el primer elemento o cualquier elemento
+            unset($tecnologias[0]);
+            ?>
+            <pre>
+              <?php print_r($tecnologias); ?>
+            </pre>
+
+
+            <?php
+               //Borra el primer elemento y lo agrega a una variable
+            $html = array_shift($tecnologias);
+            ?>
+            <h1><?php echo $html; ?></h1>
+            <pre>
+              <?php print_r($tecnologias); ?>
+            </pre>
+
+            <?php //Remover ciertos elementos y agregar otos, el elemento eliminado lo asignamos a una variable ?>
+            <?php $array = array_splice($tecnologias, 1, 1, array('AngularJS','JQuery','Ionic')); ?>
+            <pre>
+              <?php print_r($array); ?>
+            </pre>
+            <pre>
+              <?php print_r($tecnologias); ?>
+            </pre>
+
         </div>
     </div>
 
 
 
- 
+
 </body>
 
-</html>
+</html> 
