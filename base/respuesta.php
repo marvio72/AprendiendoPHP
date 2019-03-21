af717726c5154f45360d0795524a76f7b4e2637c
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-03-20 21:46:36 -0600

    Leyendo e imprimiendo los valores de un POST

---
+++ base/respuesta.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Aprendiendo PHP</title>
+    <link href="https://fonts.googleapis.com/css?family=Proza+Libre" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/estilos.css" media="screen" title="no title">
+</head>
+
+<body>
+
+    <div class="contenedor">
+        <h1>Aprendiendo PHP</h1>
+
+        <?php $resultado = $_POST; ?>
+
+        <p>Nombre: <?php echo $resultado['nombre'];  ?></p>
+        <p>Apellido: <?php echo $resultado['apellido'];  ?></p>
+
+
+    </div>
+
+
+
+
+</body>
+
+</html> 
